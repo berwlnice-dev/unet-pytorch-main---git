705541a56e105d579d268196726f8b7677f0d4db
berwlnice-dev <berwlnice@gmail.com>
2025-12-16 20:36:47 +0800

    Implement CBAM attention module in UNet
    Added CBAM (Convolutional Block Attention Module) with channel and spatial attention classes to enhance feature representation in the UNet architecture.

--- nets/unet.py
+++ nets/unet.py
@@ -4,6 +4,50 @@ import torch.nn as nn
 from nets.resnet import resnet50
 from nets.vgg import VGG16
 
+# --- 大哥，这是我为您准备的 CBAM 注意力模块 ---
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class CBAM(nn.Module):
+    def __init__(self, in_planes):
+        super(CBAM, self).__init__()
+        self.ca = ChannelAttention(in_planes)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        out = x * self.ca(x)
+        result = out * self.sa(out)
+        return result
+# -------------------------------------------
+
+
+
 
 class unetUp(nn.Module):
     def __init__(self, in_size, out_size):
@@ -12,6 +56,7 @@ class unetUp(nn.Module):
         self.conv2  = nn.Conv2d(out_size, out_size, kernel_size = 3, padding = 1)
         self.up     = nn.UpsamplingBilinear2d(scale_factor = 2)
         self.relu   = nn.ReLU(inplace = True)
+        self.cbam   = CBAM(out_size)
 
     def forward(self, inputs1, inputs2):
         outputs = torch.cat([inputs1, self.up(inputs2)], 1)
@@ -19,6 +64,7 @@ class unetUp(nn.Module):
         outputs = self.relu(outputs)
         outputs = self.conv2(outputs)
         outputs = self.relu(outputs)
+        outputs = self.cbam(outputs)
         return outputs
 
 class Unet(nn.Module):
